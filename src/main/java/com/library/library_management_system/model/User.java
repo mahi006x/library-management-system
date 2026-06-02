90789f7d49f4dada2e5685ba11c155da4e93c8e2
mahi006x <bindumahendra9966@gmail.com>
2026-06-02 18:16:01 +0530

    Initial Spring Boot setup with User entity and repository

---
+++ src/main/java/com/library/library_management_system/model/User.java
@@ -0,0 +1,30 @@
+package com.library.library_management_system.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String role;
+
+    private LocalDate joinedDate;
+}
